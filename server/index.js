7189100bed36e4353dcf3060d4e7b03dbe9a7f55
Thang Khai <tnkhai94@gmail.com>
2020-09-10 16:17:50 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require ('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const authCtrl = require('./controllers/authController')
+
+const PORT = 4000
+
+const {SESSION_SECRET, CONNECTION_STRING} = process.env
+
+const app = express()
+
+app.use(express.json())
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log('db connected')
+})
+
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET,
+    })
+)
+
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout)
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
