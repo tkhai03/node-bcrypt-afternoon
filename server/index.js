9b324cb83e86b73e3cb27a57261ae8f8aa373aa7
Thang Khai <tnkhai94@gmail.com>
2020-09-11 09:46:10 -0600

    p3 frontend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const session = require('express-session')
 const massive = require('massive')
 const authCtrl = require('./controllers/authController')
+const treasureCtrl = require('./controllers/treasureController')
+const auth = require('./middleware/authMiddleware')
 
 const PORT = 4000
 
@@ -31,5 +33,9 @@ app.use(
 app.post('/auth/register', authCtrl.register)
 app.post('/auth/login', authCtrl.login)
 app.get('/auth/logout', authCtrl.logout)
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', auth.usersOnly, treasureCtrl.getAllTreasure)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
